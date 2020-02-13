c6716e3b4853ccc96a904dc50582bd879d0c1774
Aneta Jagla <anett@poczta.fm>
2020-02-13 23:53:33 +0100

    Updates

---
+++ precache-manifest.764f9adfeabaee2ac45be718207de323.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfc36ba76d957f2b5b18de9ddaed7459",
+    "url": "/currencyConverter-React/index.html"
+  },
+  {
+    "revision": "ea71db2210bd1d396c1b",
+    "url": "/currencyConverter-React/static/css/main.80ea789a.chunk.css"
+  },
+  {
+    "revision": "3779444c6ab72e998a57",
+    "url": "/currencyConverter-React/static/js/2.306fe892.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/currencyConverter-React/static/js/2.306fe892.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ea71db2210bd1d396c1b",
+    "url": "/currencyConverter-React/static/js/main.d84b1e85.chunk.js"
+  },
+  {
+    "revision": "342dc6b7c836f5223fbc",
+    "url": "/currencyConverter-React/static/js/runtime-main.48ac1fd1.js"
+  }
+]);
